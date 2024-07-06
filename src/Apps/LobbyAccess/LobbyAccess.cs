bbb10421c0a7988ad9e0b5c526d0a954d2da9ff4
Coppermine-SP <son5121502@gmail.com>
2024-07-07 03:02:25 +0900

    feat: LobbyAccess App (공동현관문 출입 알림)

---
+++ src/Apps/LobbyAccess/LobbyAccess.cs
@@ -0,0 +1,29 @@
+﻿using PacketDotNet;
+
+namespace CloudInteractive.HomNetBridge.Apps.LobbyAccess
+{
+    [NetDaemonApp]
+    public class LobbyAccess : HomNetAppBase
+    {
+        private IHaContext _context;
+
+        public LobbyAccess(IHaContext context, ILogger<LobbyAccess> logger, IEthernetCapture netCapture) : base(logger, ethernetCapture: netCapture) => _context = context;
+
+        [Protocol(ProtocolType.Udp)]
+        [Contains("CallType=TYPE_LOBBY, MsgType=reqOpenDoor")]
+        public void LobbyOpenRequested(IPPacket packet)
+        {
+            Logger.LogInformation($"Lobby reqOpenDoor event detected in HBM REPORT!");
+            Notification.SendNotification(_context,"공동현관문", $"공동현관문 출입 요청을 승인했습니다.", Notification.NotifyLevel.TimeSensitive, tag: "lobbyAccess");
+        }
+
+        [Protocol(ProtocolType.Udp)]
+        [Contains("CallType=TYPE_LOBBY, MsgType=evtRing")]
+        public void LobbyRingEvent(IPPacket packet)
+        {
+            Logger.LogInformation($"Lobby evtRing event detected in HBM REPORT!");
+            Notification.SendNotification(_context, "공동현관문", $"공동현관문 출입 요청이 있습니다.", Notification.NotifyLevel.TimeSensitive, tag:"lobbyAccess");
+        }
+
+    }
+}
