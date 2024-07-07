4d51bdbec02d619806a396d089a850770d768696
Coppermine-SP <son5121502@gmail.com>
2024-07-07 18:48:47 +0900

    fix: Rule이 잘못된 파라미터를 가짐

--- src/Apps/LobbyAccess/LobbyAccess.cs
+++ src/Apps/LobbyAccess/LobbyAccess.cs
@@ -11,7 +11,7 @@ namespace CloudInteractive.HomNetBridge.Apps.LobbyAccess
 
         [Protocol(ProtocolType.Udp)]
         [Contains("CallType=TYPE_LOBBY, MsgType=reqOpenDoor")]
-        public void LobbyOpenRequested(IPPacket packet)
+        public void LobbyOpenRequested(IEthernetCapture.EthernetReceiveEventArgs e)
         {
             Logger.LogInformation($"Lobby reqOpenDoor event detected in HBM REPORT!");
             Notification.SendNotification(_context,"공동현관문", $"공동현관문 출입 요청을 승인했습니다.", Notification.NotifyLevel.TimeSensitive, tag: "lobbyAccess");
@@ -19,7 +19,7 @@ namespace CloudInteractive.HomNetBridge.Apps.LobbyAccess
 
         [Protocol(ProtocolType.Udp)]
         [Contains("CallType=TYPE_LOBBY, MsgType=evtRing")]
-        public void LobbyRingEvent(IPPacket packet)
+        public void LobbyRingEvent(IEthernetCapture.EthernetReceiveEventArgs e)
         {
             Logger.LogInformation($"Lobby evtRing event detected in HBM REPORT!");
             Notification.SendNotification(_context, "공동현관문", $"공동현관문 출입 요청이 있습니다.", Notification.NotifyLevel.TimeSensitive, tag:"lobbyAccess");
