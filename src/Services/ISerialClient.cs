9d15ae621ad254d2a67cf50005a5ac0107be1b3e
Coppermine-SP <son5121502@gmail.com>
2024-07-09 01:07:02 +0900

    chore: 파일 구조 개선, NullSerialClient

--- src/Services/ISerialClient.cs
+++ src/Services/ISerialClient.cs
@@ -12,5 +12,19 @@
         }
     }
 
+    public class NullSerialClient : ISerialClient
+    {
+        private readonly ILogger _logger;
+
+        public NullSerialClient(ILogger<NullEthernetCapture> logger)
+        {
+            _logger = logger;
+            _logger.LogWarning("Using NullSerialClient. Use test purpose only.");
+        }
+
+        public void SendAsync(string hex) => _logger.LogInformation($"Send => {hex}");
+        public event EventHandler<ISerialClient.SerialReceiveEventArgs>? ReceivedEvent;
+    }
+
 
 }
