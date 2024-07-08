a5f4b37892a678a3342ab0aea477451a596fbf7a
copperminesp <son5121502@gmail.com>
2024-07-08 15:13:36 +0900

    add: LocalSerialClient (로컬 COM 포트 통신), WIP

--- src/Services/LocalSerialClient.cs
+++ src/Services/LocalSerialClient.cs
@@ -1,11 +1,56 @@
-﻿namespace CloudInteractive.HomNetBridge.Services
+﻿using System.IO.Ports;
+using Microsoft.Extensions.Configuration;
+
+namespace CloudInteractive.HomNetBridge.Services
 {
     public class LocalSerialClient : ISerialClient
     {
-        public void SendAsync(string content) => throw new NotImplementedException();
+        private readonly ILogger<LocalSerialClient> _logger;
+        private readonly SerialPort _port;
+        public LocalSerialClient(ILogger<LocalSerialClient> logger, IConfiguration config)
+        {
+            _logger = logger;
+            string interfaces = "Available serial devices:\n";
+            foreach (string x in SerialPort.GetPortNames())
+            {
+                interfaces += $"{x}\n";
+            }
+            _logger.LogInformation(interfaces);
 
+            var section = config.GetSection("LocalSerialClient");
+            string? interfaceName = section.GetValue<string>("InterfaceName");
+
+            if (interfaceName is null)
+            {
+                _logger.LogWarning("InterfaceName is not configured!");
+                throw new NullReferenceException("InterfaceName was null.");
+            }
+            
+            _logger.LogInformation($"Open device {interfaceName}...");
+            _port = new SerialPort(interfaceName)
+            {
+                BaudRate = 9600,
+                DataBits = 8,
+                StopBits = StopBits.One,
+                Parity = Parity.None,
+                DiscardNull = true
+            };
+            _port.DataReceived += PortOnDataReceived;
+            _port.ErrorReceived += PortOnErrorReceived;
+            _port.Open();
+        }
+        ~LocalSerialClient() => _port.Close();
+        
+        private void PortOnErrorReceived(object sender, SerialErrorReceivedEventArgs e) => _logger.LogWarning($"Error on Serial device {_port.PortName}: {e.EventType.ToString()}");
+        private void PortOnDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            
+        }
+
+        public void SendAsync(string content)
+        {
+            
+        }
         public event EventHandler<ISerialClient.SerialReceiveEventArgs>? ReceivedEvent;
-
-        //TODO: Serial communication via USB Adapter.
     }
 }
